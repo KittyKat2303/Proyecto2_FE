230aa3c237f29c5d549205e204255f058d6422f8
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-10-02 16:41:28 -0600

    add site css

--- Proyecto1_KatherineMurillo/Controllers/HomeController.cs
+++ Proyecto1_KatherineMurillo/Controllers/HomeController.cs
@@ -16,7 +16,11 @@ namespace Proyecto1_KatherineMurillo.Controllers
         public IActionResult Index()
         {
             return View();
-        }       
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
