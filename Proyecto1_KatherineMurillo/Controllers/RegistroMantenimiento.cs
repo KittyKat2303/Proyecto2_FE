118184745ce92b0784c5a6e72aa86c92baadef6d
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-10-05 11:32:56 -0600

    controller mantenimiento

--- Proyecto1_KatherineMurillo/Controllers/RegistroMantenimiento.cs
+++ Proyecto1_KatherineMurillo/Controllers/RegistroMantenimiento.cs
@@ -14,7 +14,7 @@ namespace Proyecto1_KatherineMurillo.Controllers
         }
 
         // GET: RegistroMantenimiento/Create
-        public ActionResult Create()
+        public ActionResult AbrirCreateMatenimiento()
         {
             return View();
         }
@@ -22,10 +22,18 @@ namespace Proyecto1_KatherineMurillo.Controllers
         // POST: RegistroMantenimiento/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult CreateMantenimiento(Mantenimiento obj_mantenimiento)
         {
             try
             {
+                if (obj_mantenimiento == null)
+                {
+                    return View();
+                }
+                else
+                {
+                    listaMantenimiento.Add(obj_mantenimiento);
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -35,18 +43,43 @@ namespace Proyecto1_KatherineMurillo.Controllers
         }
 
         // GET: RegistroMantenimiento/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult AbrirEditMantenimiento(int id)
         {
+            if (listaMantenimiento.Any())
+            {
+                Mantenimiento mantenimientoedit = listaMantenimiento.FirstOrDefault(mantenimientoedit => mantenimientoedit.IdMantenimiento == id);
+                return View(mantenimientoedit);
+            }
             return View();
         }
 
         // POST: RegistroMantenimiento/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult EditMantenimiento(Mantenimiento obj_mantenimientoedit)
         {
             try
             {
+                if (listaMantenimiento.Any())
+                {
+                    Mantenimiento mantenimientoedit = listaMantenimiento.FirstOrDefault(mantenimiento => mantenimiento.IdMantenimiento == obj_mantenimientoedit.IdMantenimiento);
+                    if (mantenimientoedit != null)
+                    {
+                        mantenimientoedit.IdMantenimiento = obj_mantenimientoedit.IdMantenimiento;
+                        mantenimientoedit.FechaEjecutado = obj_mantenimientoedit.FechaEjecutado;
+                        mantenimientoedit.FechaAgendado = obj_mantenimientoedit.FechaAgendado;
+                        mantenimientoedit.MetrosPropiedad = obj_mantenimientoedit.MetrosPropiedad;
+                        mantenimientoedit.MetrosCercaViva = obj_mantenimientoedit.MetrosCercaViva;
+                        mantenimientoedit.DiasSinChapia = obj_mantenimientoedit.DiasSinChapia;
+                        mantenimientoedit.FechaOtraChapia = obj_mantenimientoedit.FechaOtraChapia;
+                        mantenimientoedit.TipoZacate = obj_mantenimientoedit.TipoZacate;
+                        mantenimientoedit.AplicacionProducto = obj_mantenimientoedit.AplicacionProducto;
+                        mantenimientoedit.ProductoAplicado = obj_mantenimientoedit.ProductoAplicado;
+                        mantenimientoedit.CostoChapia = obj_mantenimientoedit.CostoChapia;
+                        mantenimientoedit.CostoAplicacionProducto = obj_mantenimientoedit.CostoAplicacionProducto;
+                        mantenimientoedit.EstadoMantenimiento = obj_mantenimientoedit.EstadoMantenimiento;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -56,18 +89,30 @@ namespace Proyecto1_KatherineMurillo.Controllers
         }
 
         // GET: RegistroMantenimiento/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult AbrirDeleteMantenimiento(int id)
         {
-            return View();
+            Mantenimiento obj_mantenimiento = listaMantenimiento.FirstOrDefault(mantenimiento => mantenimiento.IdMantenimiento == id);
+
+            if (obj_mantenimiento == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj_mantenimiento);
         }
 
         // POST: RegistroMantenimiento/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult DeleteMantenimiento(Mantenimiento obj_mantenimiento)
         {
             try
             {
+                Mantenimiento obj_mantenimientodelete = listaMantenimiento.FirstOrDefault(mantenimiento => mantenimiento.IdMantenimiento == obj_mantenimiento.IdMantenimiento);
+
+                if (obj_mantenimientodelete != null)
+                {
+                    listaMantenimiento.Remove(obj_mantenimientodelete);
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
