4e5ccfeb2fd53418e69c1030ea2f97019d608d8a
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-10-27 16:10:25 -0600

    product controller

--- Proyecto1_KatherineMurillo/Controllers/ProductController.cs
+++ Proyecto1_KatherineMurillo/Controllers/ProductController.cs
@@ -1,17 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto1_KatherineMurillo.Models;
 
 namespace Proyecto1_KatherineMurillo.Controllers
 {
     public class ProductController : Controller
     {
-
+        #region VARIABLE PRIVADA
+    
+        #endregion
         #region EVENTO DE APERTURA VIEW
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            cls_GestorCNXApis Obj_CNX = new cls_GestorCNXApis();
+            List<cls_Product> lstResultado = await Obj_CNX.ListarProduct();
+            return View(lstResultado);
+        }
+        public IActionResult AbrirCrearProduct() 
         {
             return View();
         }
+        public async Task<IActionResult> AbrirModificarProduct(int _iId_Product)  
+        {
+            cls_GestorCNXApis Obj_Gestor = new cls_GestorCNXApis();   //INSTANCIO OBJ DE LA CLASE GESTORCONEX
+            List<cls_Product> _lstResultado = await Obj_Gestor.ConsultarProduct(new cls_Product { iId = _iId_Product });
+            cls_Product Obj_Encontrado = _lstResultado.FirstOrDefault();  //ENCUENTRA EL PRIMER DATO DE LA LISTA
+            return View(Obj_Encontrado);
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> AbrirEliminarProduct(int _iId_Product)
+        {
+            cls_GestorCNXApis Obj_Gestor = new cls_GestorCNXApis();
+            await Obj_Gestor.EliminarProduct(new cls_Product { iId = _iId_Product });
+            return RedirectToAction("Index", "Product");
+        }
         #endregion
 
+        #region EVENTOS MANTENIMIENTOS
+        [HttpPost]
+        public async Task<IActionResult> InsertProduct(cls_Product Obj_Product)
+        {
+            cls_GestorCNXApis Obj_Gestor = new cls_GestorCNXApis();
+            await Obj_Gestor.AgregarProduct(Obj_Product);
+            return RedirectToAction("Index", "Product");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(cls_Product Obj_Product)
+        {
+            cls_GestorCNXApis Obj_Gestor = new cls_GestorCNXApis();
+            await Obj_Gestor.ModificarProduct(Obj_Product);
+            return RedirectToAction("Index", "Product");
+        }
+
+        #endregion
     }
 }
--- Proyecto1_KatherineMurillo/Controllers/cls_GestorCNXApis.cs
+++ Proyecto1_KatherineMurillo/Controllers/cls_GestorCNXApis.cs
@@ -1,7 +1,93 @@
-﻿namespace Proyecto1_KatherineMurillo.Controllers
+﻿using Proyecto1_KatherineMurillo.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Proyecto1_KatherineMurillo.Controllers
 {
     public class cls_GestorCNXApis
     {
+        #region VARIABLE PÚBLICAS
+        public HttpClient hcCNXApi;
+        public cls_GestorCNXApis() 
+        {
+            hcCNXApi = new HttpClient();
+            EstablecerConexion();
+        }
+        #endregion
 
+        #region METODOS
+
+        #region PRIVADOS
+        private void EstablecerConexion()                              // AL OBJ CONEXIONAPIS LE DEBO DE ESTABLECER CIERTOS DATOS
+        {                                                                     // INDICAR QUE EL TRASLADO DE DATOS VA EN FORMATO JSON Y QUE CUAL ES LA DIRECCION BASE AL QUE VA APUNTAR
+            hcCNXApi.BaseAddress = new Uri("http://localhost:35464");  // EL URI AGARRA LA RUTA BASE  DEL API PUEDE SER IIS O DEL HOST API
+            hcCNXApi.DefaultRequestHeaders.Accept.Clear();            //SE LIMPIAN LOS VALORES POR DEFECTO   
+            hcCNXApi.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        #endregion
+
+        #region PUBLICOS
+
+        #region PRODUCT
+        public async Task<List<cls_Product>> ListarProduct()
+        {
+            List<cls_Product> Obj_lstResultado = new List<cls_Product>();
+            string _sRutaAPI = @"api/Product/ListarProduct";
+
+            HttpResponseMessage resultadoconsumo = await hcCNXApi.GetAsync(_sRutaAPI);
+            if (resultadoconsumo.IsSuccessStatusCode)
+            {
+                string jsonstring = await resultadoconsumo.Content.ReadAsStringAsync();
+                Obj_lstResultado = JsonSerializer.Deserialize<List<cls_Product>>(jsonstring);
+            }
+            return Obj_lstResultado;
+        }
+        public async Task<List<cls_Product>> ConsultarProduct(cls_Product P_Entidad)
+        {
+            List<cls_Product> Obj_lstResultado = new List<cls_Product>();
+            string _sRutaAPI = @"api/Product/FiltrarProduct";
+
+            hcCNXApi.DefaultRequestHeaders.Add("id", P_Entidad.iId.ToString());
+
+            HttpResponseMessage resultadoconsumo = await hcCNXApi.GetAsync(_sRutaAPI);
+            if (resultadoconsumo.IsSuccessStatusCode)
+            {
+                string jsonstring = await resultadoconsumo.Content.ReadAsStringAsync();
+                Obj_lstResultado = JsonSerializer.Deserialize<List<cls_Product>>(jsonstring);
+            }
+            return Obj_lstResultado;
+        }
+
+
+        public async Task<bool> AgregarProduct(cls_Product P_Entidad)
+        {
+            string _sRutaAPI = @"api/Product/AgregarProduct";    //VA A CONCATENAR A LA RUTA BASE     
+
+            HttpResponseMessage resultadoconsumo = await hcCNXApi.PostAsJsonAsync(_sRutaAPI, P_Entidad);
+            return resultadoconsumo.IsSuccessStatusCode;
+        }
+        public async Task<bool> ModificarProduct(cls_Product P_Entidad)
+        {
+            string _sRutaAPI = @"api/Product/ModificarProduct";    //VA A CONCATENAR A LA RUTA BASE     
+
+            hcCNXApi.DefaultRequestHeaders.Add("id", P_Entidad.iId.ToString());        //HAY UN PARAMETRO "id" DEL CONTROLLER DE LA SOLUCION BASE
+
+            HttpResponseMessage resultadoconsumo = await hcCNXApi.PutAsJsonAsync(_sRutaAPI, P_Entidad);
+            return resultadoconsumo.IsSuccessStatusCode;
+        }
+        public async Task<bool> EliminarProduct(cls_Product P_Entidad)
+        {
+            string _sRutaAPI = @"api/Product/EliminarProduct";    //VA A CONCATENAR A LA RUTA BASE     
+
+            hcCNXApi.DefaultRequestHeaders.Add("id", P_Entidad.iId.ToString());        //HAY UN PARAMETRO "id" DEL CONTROLLER DE LA SOLUCION BASE
+
+            HttpResponseMessage resultadoconsumo = await hcCNXApi.DeleteAsync(_sRutaAPI);
+            return resultadoconsumo.IsSuccessStatusCode;
+        }
+        #endregion
+
+        #endregion
+
+        #endregion
     }
 }
