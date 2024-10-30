1a655fe6bc71fc616c37d11014c0ed8041ef84d3
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-10-30 17:20:10 -0600

    filtrar empleado

--- Proyecto1_KatherineMurillo/wwwroot/js/site.js
+++ Proyecto1_KatherineMurillo/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var input = document.querySelectorAll('input');         //FUNCION AUTO LENGTH
+for (i = 0; i < input.length; i++) {
+    input[i].setAttribute('size', input[i].getAttribute('placeholder').length);
+}
+
+
+function validarFormulario() {
+
+    const feedbackElement = document.getElementById('estadoFeedback');
+    valor = document.getElementById("estado").value;
+    if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
+        return false;
+        feedbackElement.textContent = "debe digitar algún estado";
+    }
+
+}
